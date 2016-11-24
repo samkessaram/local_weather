36b2f93f4e8b2b70c04eb8c6f1a268b9862faa0a
samkessaram <samkessaram@gmail.com>
2016-11-24 13:07:47 +0000

    pulled in js from codepen

--- scripts.js
+++ scripts.js
@@ -1,12 +1,55 @@
 $(function(){
 
-  var wunderground = 'https://api.wunderground.com/api/1f82a733ebea4fe0/geolookup/conditions/q/autoip.json'
-  var city;
+  var wunderground = 'https://api.wunderground.com/api/1f82a733ebea4fe0/geolookup/conditions/astronomy/q/autoip.json'
   var forecast;
+  var sunTimes;
+  var celsius = true;
+  // var kph = true;
+  
   $.getJSON(wunderground, function(response){
-    city = response.current_observation.display_location.full;
     forecast = response.current_observation;
+    sunTimes = response.moon_phase;
+    inputForecastData(forecast);
   })
 
+  function  inputForecastData(){
+    $('#city').html(forecast.display_location.full);
+    pickIcon(forecast.icon);
+    $('#temperature').html(forecast.temp_c);
+    $('#feels-like').html(forecast.feelslike_c);
+    $('#conditions').html(forecast.icon);
+    $('#wind-chill').html(forecast.windchill_c);
+    $('#wind-speed').html(forecast.wind_kph);
+    $('#wind-gust').html(forecast.wind_gust_kph);
+    $('#wind-direction').html(forecast.wind_dir);
+    $('#humidity').html(forecast.relative_humidity);
+    
+  }
+
+  function pickIcon(icon){
+    console.log(sunUp());
+    if (sunUp()){
+      console.log("Sun is shinin'");
+    } else {
+      console.log("Sun is down");
+    }
+    
+    
+    $('#icon').html('<i class="wi wi-' + icon + '"></i>');
+  }
+
+  function sunUp(){
+    var timeNow = new Date();
+    var sunrise = new Date();
+    var sunset = new Date();
+    
+    sunrise.setHours(sunTimes.sunrise.hour);
+    sunrise.setMinutes(sunTimes.sunrise.minute);
+    
+    sunset.setHours(sunTimes.sunset.hour);
+    sunset.setMinutes(sunTimes.sunset.minute);
+    
+    return timeNow > sunrise && timeNow < sunset;
+  }
 
 });
