e75bf64ca98badea6d5d3a39bbae94defdb1cec5
samkessaram <samkessaram@gmail.com>
2016-11-26 14:18:34 +0000

    added orientation change fn to create new background

--- background.js
+++ background.js
@@ -1,67 +1,80 @@
-  function setBackground(conditions,sun){
-    var xColors;
-    var yColors;
-    var day = ['#ACF0F2','#F3FFE2','#FFFFFF'];
-    var night = ['#130523','#090658','#0040A4'];
+var xColors;
+var yColors;
 
-    var sunny = ['#FF9800','#FFF3AE','#FFC305'];
-    var snow = ['#FFFFFF','#DEDEDE','#77E0F6','#B5B5B5','#A2F2F6'];
-    var storm = ['#130029','#F3CE2F','#180033','#9B83B5'];
-    var rain = ['#49668C','#72808C','#2D3359'];
-    var cloudy = ['#263248','#D1DBBD','#7E8AA2','#263248'];
-    var partlycloudy = ['#F3E565','#7E8AA2']
+function setBackground(conditions,sun){
+  var day = ['#ACF0F2','#F3FFE2','#FFFFFF'];
+  var night = ['#130523','#090658','#0040A4'];
 
-    if(sun){
-      yColors = day;
-      $('#celsius').addClass('day-color');
-      $('#fahrenheit').addClass('day-color');
-    } else {
-      yColors = night;
-      $('body').css('color','white');
-      $('#celsius').addClass('night-color');
-      $('#fahrenheit').addClass('night-color');
-    }
+  var sunny = ['#FF9800','#FFF3AE','#FFC305'];
+  var snow = ['#FFFFFF','#DEDEDE','#77E0F6','#B5B5B5','#A2F2F6'];
+  var storm = ['#130029','#F3CE2F','#180033','#9B83B5'];
+  var rain = ['#49668C','#72808C','#2D3359'];
+  var cloudy = ['#263248','#D1DBBD','#7E8AA2','#263248'];
+  var partlycloudy = ['#F3E565','#7E8AA2']
 
-    switch(conditions){
-      case 'sunny':
-      case 'mostlysunny':
-      case 'clear':
-        xColors = sunny;
-        break;
-      case 'snow':
-      case 'flurries':
-      case 'chanceflurries':
-      case 'chancesnow':
-        xColors = snow;
-        break;
-      case 'sleat':
-      case 'chancerain':
-      case 'rain':
-        xColors = rain;
-        break;
-      case 'storm-showers':
-      case 'tstorms':
-        xColors = storm;
-        break;
-      case 'partlycloudy':
-        xColors = partlycloudy;
-        break;
-      case 'cloudy':
-      case 'mostlycloudy':
-        xColors = cloudy;
-        break;
-      default:
-        xColors = cloudy;
-    }
-
-    var pattern = Trianglify({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      cell_size: 200,
-      x_colors: xColors,
-      y_colors: yColors
-    })
-
-    pattern.canvas(document.getElementById('canvas'));
-    $('body').show();
+  if(sun){
+    yColors = day;
+    $('#celsius').addClass('day-color');
+    $('#fahrenheit').addClass('day-color');
+  } else {
+    yColors = night;
+    $('body').css('color','white');
+    $('#celsius').addClass('night-color');
+    $('#fahrenheit').addClass('night-color');
   }
+
+  switch(conditions){
+    case 'sunny':
+    case 'mostlysunny':
+    case 'clear':
+      xColors = sunny;
+      break;
+    case 'snow':
+    case 'flurries':
+    case 'chanceflurries':
+    case 'chancesnow':
+      xColors = snow;
+      break;
+    case 'sleat':
+    case 'chancerain':
+    case 'rain':
+      xColors = rain;
+      break;
+    case 'storm-showers':
+    case 'tstorms':
+      xColors = storm;
+      break;
+    case 'partlycloudy':
+      xColors = partlycloudy;
+      break;
+    case 'cloudy':
+    case 'mostlycloudy':
+      xColors = cloudy;
+      break;
+    default:
+      xColors = cloudy;
+  }
+
+  var pattern = Trianglify({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    cell_size: 200,
+    x_colors: xColors,
+    y_colors: yColors
+  })
+
+  pattern.canvas(document.getElementById('canvas'));
+  $('body').show();
+}
+
+$(window).on("orientationchange",function(){
+  var pattern = Trianglify({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    cell_size: 200,
+    x_colors: xColors,
+    y_colors: yColors
+  })
+
+  pattern.canvas(document.getElementById('canvas'));
+});
