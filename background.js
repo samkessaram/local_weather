caefc9a1cbe0ae9cd261bd67932f548e30601715
samkessaram <samkessaram@gmail.com>
2016-11-29 11:33:10 +0000

    refactored background generation function

--- background.js
+++ background.js
@@ -2,22 +2,13 @@ var xColors;
 var yColors;
 
 function setBackground(weather,sun){
-  var day = ['#ACF0F2','#F3FFE2','#FFFFFF'];
-  var night = ['#130523','#090658','#0040A4'];
-
-  var sunny = ['#FF9800','#FFF3AE','#FFC305'];
-  var snow = ['#FFFFFF','#DEDEDE','#77E0F6','#B5B5B5','#A2F2F6'];
-  var storm = ['#130029','#F3CE2F','#180033','#9B83B5'];
-  var rain = ['#49668C','#72808C','#2D3359'];
-  var cloudy = ['#263248','#D1DBBD','#7E8AA2','#263248'];
-  var partlycloudy = ['#F3E565','#7E8AA2']
 
   if(sun){
-    yColors = day;
+    yColors = ['#ACF0F2','#F3FFE2','#FFFFFF'];
     $('#celsius').addClass('day-color');
     $('#fahrenheit').addClass('day-color');
   } else {
-    yColors = night;
+    yColors = ['#130523','#090658','#0040A4'];
     $('body').css('color','white');
     $('#celsius').addClass('night-color');
     $('#fahrenheit').addClass('night-color');
@@ -27,32 +18,29 @@ function setBackground(weather,sun){
     case 'sunny':
     case 'mostlysunny':
     case 'clear':
-      xColors = sunny;
+      xColors = ['#FF9800','#FFF3AE','#FFC305'];
       break;
     case 'snow':
     case 'flurries':
     case 'chanceflurries':
     case 'chancesnow':
-      xColors = snow;
+      xColors = ['#FFFFFF','#DEDEDE','#77E0F6','#B5B5B5','#A2F2F6'];
       break;
     case 'sleat':
     case 'chancerain':
     case 'rain':
-      xColors = rain;
+      xColors = ['#49668C','#72808C','#2D3359'];
       break;
     case 'storm-showers':
     case 'tstorms':
-      xColors = storm;
-      break;
-    case 'partlycloudy':
-      xColors = partlycloudy;
+      xColors = ['#130029','#F3CE2F','#180033','#9B83B5'];
       break;
     case 'cloudy':
     case 'mostlycloudy':
-      xColors = cloudy;
+      xColors = ['#263248','#D1DBBD','#7E8AA2','#263248'];
       break;
     default:
-      xColors = cloudy;
+      xColors = ['#F3E565','#7E8AA2'];
   }
 
   var pattern = Trianglify({
