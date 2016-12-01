5eb9b7da75d35fd0549f8f032bfc91dd3a895b45
samkessaram <samkessaram@gmail.com>
2016-12-01 23:10:32 +0000

    updated display str for unknown conditions

--- grammar.js
+++ grammar.js
@@ -26,7 +26,7 @@ function upcase(str){
   if ( str[0] ){
     str[0] = str[0].toUpperCase();
   } else {
-    return 'Unknown';         // For the case of unknown conditons.
+    return '-';         // For the case of unknown conditons.
   }
   str = str.join('');
   return str;
