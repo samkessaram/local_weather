33ffd404fcb813b033878899bfb9d7bf651db560
samkessaram <samkessaram@gmail.com>
2016-12-23 18:30:27 +0000

    added 3 day forecast

--- forecast.js
+++ forecast.js
@@ -1,12 +1,17 @@
 function inputForecast(forecast){
-  console.log(forecast);
+  // console.log(forecast);
   var threeDay = []
   for (var i = 1; i < forecast.length; i++){
     var data = forecast[i];
     var day = {
       "day": data.date.weekday,
-      "conditions": chooseIcon(data.icon,true),
+      "conditions": parseConditions(data.icon),
+      "icon": chooseIcon(data.icon,true),
+      "c": data.high.celsius + '&deg;C',
+      "f": data.high.fahrenheit + '&deg;F',
     };
+
+    $('#day-' + i).html('<p>' + day.day + '</p><p><i class="wi ' + day.icon + '"></i></p><p>'+ day.conditions +'</p><p>' + day.c + '</p>');
     console.log(day);
   }
 }
