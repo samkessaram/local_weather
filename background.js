0e575afd977d0eb92e63371e491be558f5956dfa
samkessaram <samkessaram@gmail.com>
2017-01-15 17:56:14 +0000

    rm console logs

--- background.js
+++ background.js
@@ -54,10 +54,6 @@ function setBackground(weather,sun){
 
   function setContainerMarginTop(){
     var cont = $('#container');
-    console.log(cont.height() < window.innerHeight)
-    console.log((window.innerHeight - cont.height())/2)
-    console.log('contheight: ' + cont.height() )
-    console.log('innerHeight: ' + window.innerHeight)
     if (cont.height() < window.innerHeight){
       cont.css('margin-top', (window.innerHeight - cont.height())/2)
     }else{
